d78f2414953057e3f62ea734a5274ec5edc96b47
Alex Pelan <alexpelan@gmail.com>
2015-09-14 22:51:00 -0400

    Finish single game parser - genericize for double/final jeopardy

--- hornung/routes/scraper.js
+++ hornung/routes/scraper.js
@@ -6,6 +6,15 @@ var router = express.Router();
 var mongoose = require('mongoose');
 
 
+var parseAnswerFromMouseoverHandler = function($clue, startString){
+		var mouseoverHandler = $clue.find("div").first().attr("onmouseover");
+		var startOfAnswer = mouseoverHandler.indexOf(startString);
+		var endOfAnswer = mouseoverHandler.indexOf("</em>")
+		console.log(startOfAnswer, startString.length, endOfAnswer);
+		var answer = mouseoverHandler.slice(startOfAnswer + startString.length, endOfAnswer);
+		return answer
+};
+
 var parseJeopardySubgame = function($, jeopardyDiv){
 	var game = {};
 	var categoryOrder = {};
@@ -20,16 +29,12 @@ var parseJeopardySubgame = function($, jeopardyDiv){
 		var value = $(element).find(".clue_value").text().trim();
 		var columnIndex = index % 6;
 		var categoryName = categoryOrder[columnIndex];
-		game[categoryName][value] = "this is a question";
 
 		var question = $(element).find(".clue_text").text().trim();
 
 		//j-archive shows the answer via an onmouseover handler, we need to parse it out to figure out the actual answer
-		var mouseoverHandler = $(element).find("div").first().attr("onmouseover");
-		var startString = '"correct_response">';
-		var startOfAnswer = mouseoverHandler.indexOf(startString);
-		var endOfAnswer = mouseoverHandler.indexOf("</em>")
-		var answer = mouseoverHandler.slice(startOfAnswer + startString.length, endOfAnswer);
+		var answer = parseAnswerFromMouseoverHandler($(element), '"correct_response">');
+
 		game[categoryName][value] = {
 			"question": question,
 			"answer": answer
@@ -41,6 +46,18 @@ var parseJeopardySubgame = function($, jeopardyDiv){
 	return game;
 };
 
+var parseFinalJeopardy = function($){
+	var result = {};
+	var categoryName = $(".final_round .category").text().trim();
+	var question = $(".final_round .clue_text").text().trim();
+	var answer = parseAnswerFromMouseoverHandler($(".final_round .category"), '\\"correct_response\\">');
+	return {
+		category: categoryName,
+		question: question,
+		answer: answer
+	}
+}
+
 router.get('/', function(req, res) {
 
 	var url = "http://www.j-archive.com/showgame.php?game_id=5002";
@@ -51,13 +68,16 @@ router.get('/', function(req, res) {
 			var gameTitle = $("#game_title").text().trim();
 			var titleTokens = gameTitle.split("-");
 
-
 			var jeopardy = parseJeopardySubgame($, $("#jeopardy_round"));
+			var doubleJeopardy = parseJeopardySubgame($, $("#double_jeopardy_round"));
+			var finalJeopardy = parseFinalJeopardy($);
 
 			res.json({
 				"show_number": titleTokens[0].trim(),
 				"air_date": titleTokens[1].trim(),
-				"jeopardy": jeopardy
+				"jeopardy": jeopardy,
+				"double_jeopardy": doubleJeopardy,
+				"final_jeopardy": finalJeopardy
 			});
 		}
 		else{
